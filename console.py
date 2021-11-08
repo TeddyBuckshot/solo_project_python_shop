ea56915ac49b7a339f88340a484da205771fedbd
Sam <samspencer2202@outlook.com>
2021-11-08 11:47:36 +0000

    variables renamed + added more to db for testing

--- console.py
+++ console.py
@@ -11,7 +11,25 @@ keyboard_repository.delete_all()
 brand1 = Brand("Glorious PC gaming race", "USA")
 brand1 = brand_repository.save(brand1)
 
+brand2 = Brand("Corsair", "USA")
+brand2 = brand_repository.save(brand2)
+
+brand3 = Brand("Razer", "USA")
+brand3 = brand_repository.save(brand3)
+
 keyboard1 = Keyboard("GMMK Pro", brand1, "75% Mechanical Keyboard", 4, 100.00, 169.99)
 keyboard1 = keyboard_repository.save(keyboard1)
+
+keyboard2 = Keyboard("GMMK", brand1, "Full-size Mechanical Keyboard", 6, 80.00, 149.99)
+keyboard2 = keyboard_repository.save(keyboard2)
+
+keyboard3 = Keyboard("GMMK TKL", brand1, "10-key-less Compact Mechanical Keyboard", 10, 85.00, 154.99)
+keyboard3 = keyboard_repository.save(keyboard3)
+
+keyboard4 = Keyboard("K70", brand2, "Full-size Gaming Keyboard", 4, 120.00, 179.99)
+keyboard4 = keyboard_repository.save(keyboard4)
+
+keyboard5 = Keyboard("Huntsman Elite", brand3, "TKL Optical-switch Mechanical Keyboard", 4, 95.00, 129.99)
+keyboard5 = keyboard_repository.save(keyboard5)
 
 
